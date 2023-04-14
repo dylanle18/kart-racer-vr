59c55b7e2c91df2824e6aa4a3d78ee75f59db4e1
dylanle18 <dylanle18@outlook.com>
2023-04-14 01:54:24 -0400

    Add Project Description

--- src/components/ProjectDescription/ProjectDescription.tsx
+++ src/components/ProjectDescription/ProjectDescription.tsx
@@ -3,13 +3,14 @@ function ProjectDescription() {
     <div>
       <h2 className="font-bold text-lg mb-1">Description</h2>
       <p className="text-base">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus
-        possimus unde, assumenda magnam distinctio iste. Vitae iste nobis
-        maiores consectetur fugit tempore blanditiis repellat, rerum, dolor
-        placeat consequatur, voluptatibus odio. Lorem, ipsum dolor sit amet
-        consectetur adipisicing elit. Inventore dignissimos eligendi qui nobis.
-        Velit, doloribus animi. Rem accusantium maiores laboriosam placeat
-        delectus ullam repellat, culpa assumenda facilis iste voluptate hic!
+        Kart Racer VR is an immersive virtual reality game that puts players in
+        the driver's seat of a racecar, with the goal of completing the track as
+        quickly as possible. Players must use their skills and reflexes to
+        navigate the track, avoiding obstacles and using power ups to gain an
+        advantage and achieve the best time. The game features lowpoly graphics
+        that transport you to a cartoonish world filled with bright colors and
+        exciting twists and turns. Players will compete against themselves by
+        honing their skills to attempt to beat their personal records.
       </p>
     </div>
   );
